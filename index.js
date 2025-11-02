dcafdaf10cdd50587749d3e0d5bc269723312df2
khandakershahi <khandakershahi@gmail.com>
2025-11-03 01:30:34 +0600

    user add

--- index.js
+++ index.js
@@ -31,6 +31,24 @@ async function run() {
     const db = client.db("smart_db");
     const productsCollection = db.collection("products");
     const bidsCollection = db.collection('bids');
+    const usersCollection = db.collection('users');
+    
+    // user create API
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        res.send({message: 'user already exist, do not need to add'})
+      }
+      else{
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+      
+    })
 
     // product realted api
     // get api all
